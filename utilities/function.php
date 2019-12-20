8c14fbe209c8d2cd51908229fbcced67e554f386
GerardDupont <antoine.balannec@gmail.com>
2019-12-20 10:55:06 +0100

    fix: fix wrong table name for request

--- utilities/function.php
+++ utilities/function.php
@@ -15,7 +15,7 @@ function createEmptyPoeple(){
 
 
 function getAllUser(){
-    $res = $GLOBALS["db"]->makeQuery("SELECT * FROM user WHERE deleted=0");
+    $res = $GLOBALS["db"]->makeQuery("SELECT * FROM people WHERE deleted=0");
     $peoples = array();
     foreach($res as $datapeople){
         $temp = new people();
@@ -30,7 +30,7 @@ function getAllGroup(){
 }
 
 function getUserFromGroup($group){
-    $res = $GLOBALS["db"]->makeQuery("SELECT * FROM user WHERE deleted=0 AND group = $group");
+    $res = $GLOBALS["db"]->makeQuery("SELECT * FROM people WHERE deleted=0 AND group = $group");
     $peoples = array();
     foreach($res as $datapeople){
         $temp = new people();
